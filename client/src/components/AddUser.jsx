2120517cf1498b1e0175872ec71020e53b874f45
codingscode <heronnestg@hotmail.com>
2023-04-07 20:38:59 -0300

    parte 9

--- client/src/components/AddUser.jsx
+++ client/src/components/AddUser.jsx
@@ -1,11 +1,33 @@
 
-import React from 'react'
+import { FormControl, FormGroup, InputLabel, Input, Typography, styled } from '@mui/material'
+
+
+
 
 
 const AddUser = () => {
 
    return (
-      <p>Olá de AddUser</p>
+      <FormGroup>
+         <Typography variant='h4' >Add User</Typography>
+         <FormControl>
+            <InputLabel>Name</InputLabel>
+            <Input  />
+         </FormControl>
+         <FormControl>
+            <InputLabel>Username</InputLabel>
+            <Input  />
+         </FormControl>
+         <FormControl>
+            <InputLabel>Email</InputLabel>
+            <Input  />
+         </FormControl>
+         <FormControl>
+            <InputLabel>Phone</InputLabel>
+            <Input  />
+         </FormControl>
+         
+      </FormGroup>
    )
 }
 
