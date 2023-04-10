dc1530552358a002ec13cdedc629d8c16efa6431
codingscode <heronnestg@hotmail.com>
2023-04-09 21:35:56 -0300

    parte 12

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,10 +1,24 @@
 
+import User from '../schema/user-schema.js'
 
-export const addUser = (request, response) => {
+
+
+
+
+export const addUser = async (request, response) => {
    const user = request.body
    
    console.log(user)
 
+   const newUser = new User(user)
+   
+   try {
+      await newUser.save()
+      response.status(201).json(newUser)
+   }
+   catch(error) {
+      response.status(409).json({ message: error.message })
+   }
 
 }
 
